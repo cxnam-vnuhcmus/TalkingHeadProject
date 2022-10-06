87cae1e4b52f51098b74adb8ec2a159e0e9ce2fa
cxnam-vnuhcmus <cxnam@fit.hcmus.edu.vn>
2022-10-06 12:59:06 +0700

    update metric

--- evaluation/evaluation_image.py
+++ evaluation/evaluation_image.py
@@ -1,5 +1,8 @@
 from skimage.metrics import structural_similarity as ssim
 from evaluation.fid import FidScore
+import cpbd
+from math import log10, sqrt
+import numpy as np
 
 def calculate_ssim(image1, image2, channel_axis=None):
     '''
@@ -13,3 +16,15 @@ def calculate_fid(image1, image2, dims=2048):
     '''
     fid = FidScore(dims=dims)
     return fid.calculate_fid_score_one_shot(image1, image2)
+
+def calculate_cpbd(image):    
+    return cpbd.compute(image)
+
+def calculate_psnr(image1, image2):
+    mse = np.mean((image1 - image2) ** 2)
+    if(mse == 0):  # MSE is zero means no noise is present in the signal .
+                  # Therefore PSNR have no importance.
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+    return psnr
