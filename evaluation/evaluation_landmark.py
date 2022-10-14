246159ca86d48d6568314738f8529783ed9d51b6
cxnam-vnuhcmus <cxnam@fit.hcmus.edu.vn>
2022-10-14 17:49:45 +0700

    eval landmark

--- evaluation/evaluation_landmark.py
+++ evaluation/evaluation_landmark.py
@@ -1,4 +1,8 @@
 import numpy as np
+import json
+from glob import glob
+from tqdm import tqdm
+import os
 
 def calculate_LMD(pred_landmark, gt_landmark, norm_distance=1.0):
     r"""Compute Landmark Distance by Normalize Mean Square Error
@@ -10,14 +14,14 @@ def calculate_LMD(pred_landmark, gt_landmark, norm_distance=1.0):
     Returns:
         LMD            :   float (↓)
     """
-    euclidean_distance = np.sqrt(np.sum((pred_landmark - gt_landmark)**2, axis=(7 - pred_landmark.ndim)))
-    norm_per_frame = np.mean(euclidean_distance, axis=(4 - pred_landmark.ndim))
-    norm_per_frame_norm = np.divide(norm_per_frame, norm_distance)
-    if (3 - pred_landmark.ndim) > 0:
-        norm_per_batch = np.mean(norm_per_frame_norm, axis=1)
-    if (2 - pred_landmark.ndim) > 0:
-        norm_all_batch = np.mean(norm_per_batch, axis=0)
-    return norm_all_batch
+    euclidean_distance = np.sqrt(np.sum((pred_landmark - gt_landmark)**2, axis=(pred_landmark.ndim - 1)))
+    norm_per_frame = np.mean(euclidean_distance, axis=(pred_landmark.ndim - 2))
+    lmd = np.divide(norm_per_frame, norm_distance)
+    if (pred_landmark.ndim - 3) >= 0:
+        lmd = np.mean(lmd, axis=(pred_landmark.ndim - 3))
+    if (pred_landmark.ndim - 4) >= 0:
+        lmd = np.mean(lmd, axis=(pred_landmark.ndim - 4))
+    return lmd
 
 def calculate_LMV(pred_landmark, gt_landmark, norm_distance=1.0):
     r"""Compute Landmark Velocity by Normalize Mean Square Error
@@ -29,12 +33,67 @@ def calculate_LMV(pred_landmark, gt_landmark, norm_distance=1.0):
     Returns:
         LMV            :   float (↓)
     """
-    velocity_pred_landmark = pred_landmark[:, 1:, :, :] - pred_landmark[:, 0:-1, :, :]
-    velocity_gt_landmark = gt_landmark[:, 1:, :, :] - gt_landmark[:, 0:-1, :, :]
-    euclidean_distance = np.sqrt(np.sum((velocity_pred_landmark - velocity_gt_landmark)**2, axis=3))
-    norm_per_frame = np.mean(euclidean_distance, axis=2)
-    norm_per_frame_norm = np.divide(norm_per_frame, norm_distance)
-    norm_per_batch = np.mean(norm_per_frame_norm, axis=1)
-    norm_all_batch = np.mean(norm_per_batch, axis=0)
-    return norm_all_batch
+    if gt_landmark.ndim == 4:
+        velocity_pred_landmark = pred_landmark[:, 1:, :, :] - pred_landmark[:, 0:-1, :, :]
+        velocity_gt_landmark = gt_landmark[:, 1:, :, :] - gt_landmark[:, 0:-1, :, :]
+    elif gt_landmark.ndim == 3:
+        velocity_pred_landmark = pred_landmark[1:, :, :] - pred_landmark[0:-1, :, :]
+        velocity_gt_landmark = gt_landmark[1:, :, :] - gt_landmark[0:-1, :, :]
+        
+    euclidean_distance = np.sqrt(np.sum((velocity_pred_landmark - velocity_gt_landmark)**2, axis=(pred_landmark.ndim - 1)))
+    norm_per_frame = np.mean(euclidean_distance, axis=(pred_landmark.ndim - 2))
+    lmv = np.divide(norm_per_frame, norm_distance)
+    if (pred_landmark.ndim - 3) >= 0:
+        lmv = np.mean(lmv, axis=(pred_landmark.ndim - 3))
+    if (pred_landmark.ndim - 4) >= 0:
+        lmv = np.mean(lmv, axis=(pred_landmark.ndim - 4))
+    return lmv
 
+def calculate_folder_landmark(folderA, folderB, eval_lmd=True, eval_lmv=True):
+    total_lmd = 0
+    total_lmv = 0
+    if eval_lmd:
+        list_lmd = []
+        folderA_data = glob(os.path.join(folderA, '**/*.json'), recursive=True)
+        for lmA_path in tqdm(folderA_data):
+            lmB_path = lmA_path.replace(folderA, folderB)
+            if os.path.exists(lmB_path):
+                with open(lmA_path, 'r') as f:
+                    lmA = np.asarray(json.load(f))
+                with open(lmB_path, 'r') as f:
+                    lmB = np.asarray(json.load(f))            
+                norm_distance = np.sqrt(np.sum((lmB[0] - lmB[16])**2, axis=0))
+                lmd = calculate_LMD(lmB, lmA, norm_distance=norm_distance)
+                list_lmd.append(lmd)
+        total_lmd = sum(list_lmd)/len(list_lmd)
+    if eval_lmv:
+        list_lmv = []
+        folderA_data = glob(os.path.join(folderA, '**/*.json'), recursive=True)
+        folderA_data_new = []
+        for data in folderA_data:
+            data_new = data[:-11]
+            if data_new not in folderA_data_new:
+                folderA_data_new.append(data[:-11])
+        folderA_data_new = sorted(folderA_data_new)
+        for lmA_path in tqdm(folderA_data_new):
+            lmB_path = lmA_path.replace(folderA, folderB)
+            if os.path.exists(lmB_path):
+                lmA = []
+                lmB = []
+                for lmA_subfolder in glob(os.path.join(lmA_path, '*.json')):
+                    with open(lmA_subfolder, 'r') as f:
+                        lmA_data = json.load(f)
+                    lmB_subfolder = lmA_subfolder.replace(folderA, folderB)
+                    if os.path.exists(lmB_subfolder):
+                        lmA.append(lmA_data)
+                        with open(lmB_subfolder, 'r') as f:
+                            lmB.append(json.load(f))
+                lmA = np.asarray(lmA)
+                lmB = np.asarray(lmB)
+                
+                norm_distance = np.sqrt(np.sum((lmB[:,0] - lmB[:,16])**2, axis=0))[:-1]
+                lmv = calculate_LMV(lmB, lmA, norm_distance=norm_distance)
+                list_lmv.append(lmv)
+        total_lmv = sum(list_lmv)/len(list_lmv)
+    return {"lmd": total_lmd,
+            "lmv": total_lmv}
