ea41d28b8edcae4f22384daf2a20e753d35416bc
cxnam-vnuhcmus <cxnam@fit.hcmus.edu.vn>
2022-12-12 13:30:17 +0700

    A2LM LSTM0

--- upload_drive.py
+++ upload_drive.py
@@ -5,10 +5,10 @@ gauth = GoogleAuth()
 gauth.CommandLineAuth()      
 drive = GoogleDrive(gauth)  
 
-upload_file_list = ['/root/TalkingHead/imaginaire/logs/2022_1012_1625_20_ampO1-ds512/epoch_00198_iteration_000210000_checkpoint.pt']
+upload_file_list = ['/root/TalkingHead/imaginaire/logs/2022_1210_1526_22_ampO1/epoch_00078_iteration_000042000_checkpoint.pt']
 for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'parents': [{'id': '1CA_1Kz6-eEpdwBciY1JxAZxHBtB2FgiE'}],
-    'title': 'epoch_00198_iteration_000210000_checkpoint.pt'
+	gfile = drive.CreateFile({'parents': [{'id': '1uNSNvJ5fDzZ5BJ4wDHdrNlDSY3igI3no'}],
+    'title': 'GI2I_e78_i42000.pt'
     })
 	# Read file and set it as the content of this instance.
 	gfile.SetContentFile(upload_file)
